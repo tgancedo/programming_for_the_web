cf075741054caf44240db1a92565753436ddd6f9
Tatiana Gancedo <tgancedo@mcad.edu>
2022-01-22 15:22:40 -0500

    modify stroke weights

--- week1/assignment/snowman.js
+++ week1/assignment/snowman.js
@@ -1,4 +1,6 @@
 let grid = undefined;
+//let stroke1 = prompt("enter a basic color name in lowercase", "gray");
+//let stroke2 = prompt("enter another basic color lowercase", "purple");
 function setup() {
   createCanvas(1000, 800);
   background("#ccc");
@@ -8,29 +10,28 @@ function draw() {
   background(grid);
   // snowman legs
   fill("#f1f1f1");
-  strokeWeight(20);
-  stroke(100, 250, 100);
+  strokeWeight(10);
+  stroke(0);
   // left leg
-  ellipse(350, 650, 200);
+  ellipse(400, 650, 100);
   // right leg
-  ellipse(650, 650, 200);
+  ellipse(600, 650, 100);
   // body
   ellipse(500, 450, 350, 400);
   // head
   ellipse(500, 200, 200);
-  // hat brim
-  stroke(100, 100, 255);
-  strokeWeight(40);
-  line(400, 120, 600, 120);
   // hat body
-  quad(400, 50, 600, 50, 550, 120, 450, 120);
+  strokeJoin(ROUND);
+  rect(400, 80, 200, 80);
   // eyes
   stroke(0);
-  strokeWeight(50);
-  point(425, 200);
-  point(575, 200);
+  strokeWeight(20);
+  point(450, 200);
+  point(550, 200);
   // mouth
   noFill();
   strokeWeight(10);
-  arc(500, 200, 80, 80, 0, HALF_PI);
+  arc(500, 230, 70, 70, radians(30), radians(150));
+  // left arm
+  
 }
