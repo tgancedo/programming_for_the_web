8dcdce0de978b35aba24a3ecad12f033ce4b5ce0
Tatiana Gancedo <tgancedo@mcad.edu>
2022-01-22 16:16:13 -0500

    add babies and fill color

--- week1/assignment/snowman.js
+++ week1/assignment/snowman.js
@@ -1,49 +1,72 @@
 let grid = undefined;
-//let stroke1 = prompt("enter a basic color name in lowercase", "gray");
-//let stroke2 = prompt("enter another basic color lowercase", "purple");
+//let fill1 = prompt("enter a basic color name in lowercase", "green");
+//let fill2 = prompt("enter another basic color lowercase", "magenta");
 function setup() {
   createCanvas(1000, 800);
-  background("#ccc");
+  background("#a7bdd5");
   grid = loadImage("images/100px_grid.png");
 }
 function draw() {
-  background(grid);
+  //background(grid);
   // snowman legs
   fill("#f1f1f1");
   strokeWeight(10);
   stroke(0);
-  // left leg
+  // snowman left leg
   ellipse(400, 650, 100);
-  // right leg
+  // snowman right leg
   ellipse(600, 650, 100);
-  // left arm
+  // snowman left arm
   stroke(0);
   strokeWeight(10);
   line(200, 500, 430, 330);
-  // right arm
-  stroke (0);
-  strokeWeight(10);
+  // snowman right arm
   line(800, 450, 430, 330);
-  // body
+  // snowman body
   ellipse(500, 450, 350, 400);
-  // head
+  // snowman head
   ellipse(500, 200, 200);
-  // hat top
+  // snowman hat top
+  fill("#cdff00");
   ellipse(500, 90, 100);
-  // hat body
+  // snowman hat body
   strokeJoin(ROUND);
   rect(400, 80, 200, 80);
-  // eyes
-  stroke(0);
+  // snowman scarf horizontal
+  fill("#ff00ff");
+  strokeJoin(ROUND);
+  rect(350, 278, 300, 70);
+  // snowman scarf vertical
+  strokeJoin(ROUND);
+  rect(500, 278, 70, 250);
+  // snowman eyes
   strokeWeight(20);
   point(450, 200);
   point(550, 200);
-  // mouth
+  // snowman mouth
   noFill();
   strokeWeight(10);
   arc(500, 230, 70, 70, radians(30), radians(150));
-  // ground
+  // left baby body
   fill("#f1f1f1");
-  noStroke();
-  rect(0, 700, 1000, 200);
+  strokeWeight(10);
+  ellipse(200, 650, 130);
+  // left baby head
+  ellipse(200, 550, 100);
+  // left baby eyes
+  strokeWeight(15);
+  point(180, 550);
+  point(220, 550);
+  // right baby body
+  strokeWeight(10);
+  ellipse(800, 650, 180);
+  // right baby head
+  ellipse(800, 520, 130);
+  // right baby eyes
+  strokeWeight(15);
+  point(770, 520);
+  point(830, 520);
+  // ground
+  strokeWeight(10);
+  rect(-5, 700, 1010, 200);
 }
