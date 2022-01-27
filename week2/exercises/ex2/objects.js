80a412e45f4ace179f2578e97d300638bcc0c67a
Tatiana Gancedo <tgancedo@mcad.edu>
2022-01-27 17:18:12 -0500

    updated short descriptions to match concatenation operator flow

--- week2/exercises/ex2/objects.js
+++ week2/exercises/ex2/objects.js
@@ -1,10 +1,10 @@
 const games = [
-  { title: 'Loopin Louie', type: 'board', numberOfPlayers: '4', rating: '7/10', shortDescription: 'Do not let a spinning plane knock down your chickens by hitting the plane away with a lever' },
-  { title: 'Connect Four', type: 'board', numberOfPlayers: '2', rating: '5/10', shortDescription: 'Take turns dropping your color of checkers in a vertical grid to connect 4 in a row' },
-  { title: 'Hungry Hungry Hippos', type: 'board', numberOfPlayers: '4', rating: '4/10', shortDescription: 'Furiously push your hungry hippo’s tail a million times to eat the most marbles' },
-  { title: 'Guess Who?', type: 'board', numberOfPlayers: '2', rating: '5/10', shortDescription: 'Guess who your opponents character is by questioning different characteristics and flip down your list of options until you have one left' }
+  { title: 'Loopin Louie', type: 'board', numberOfPlayers: '4', rating: '7/10', shortDescription: 'prevent a spinning plane from knocking down their chickens.' },
+  { title: 'Connect Four', type: 'board', numberOfPlayers: '2', rating: '5/10', shortDescription: 'take turns dropping checkers in a vertical grid to connect 4 in a row.' },
+  { title: 'Hungry Hungry Hippos', type: 'board', numberOfPlayers: '4', rating: '4/10', shortDescription: 'furiously push hungry hippo’s tails a million times to eat the most marbles.' },
+  { title: 'Guess Who?', type: 'board', numberOfPlayers: '2', rating: '5/10', shortDescription: 'guess who their opponents character is by questioning different characteristics.' }
 ];
 console.log(games);
 
 const promptResponse = window.prompt("I have 4 childhood games in my collection. Pick a number between 1 and 4 and I'll tell you about that game");
-window.alert('You selected ' + games[promptResponse - 1].title + ' which is a ' + games[promptResponse - 1].type + 'game');
+window.alert('You selected ' + games[promptResponse - 1].title + ' which is a ' + games[promptResponse - 1].rating + ' rating ' + games[promptResponse - 1].type + ' game' + ' where ' + games[promptResponse - 1].numberOfPlayers + ' players ' + games[promptResponse - 1].shortDescription);
