539766c0cc510567d08fbf6b4ac8e08062d38ef2
Tatiana Gancedo <tgancedo@mcad.edu>
2022-01-27 15:22:27 -0500

    create array of objects

--- week2/exercises/ex2/objects.js
+++ week2/exercises/ex2/objects.js
@@ -1,2 +1,7 @@
-const myGames = ['Loopin Louie', 'Connect Four', 'Hungry Hungry Hippos', 'Guess Who'];
-console.log(myGames.toString());
+const games = [
+  { title: 'Loopin Louie', type: 'board', numberOfPlayers: '4', rating: '7/10', shortDescription: 'Do not let a spinning plane knock down your chickens by hitting the plane away with a lever' },
+  { title: 'Connect Four', type: 'board', numberOfPlayers: '2', rating: '5/10', shortDescription: 'Take turns dropping your color of checkers in a vertical grid to connect 4 in a row' },
+  { title: 'Hungry Hungry Hippos', type: 'board', numberOfPlayers: '4', rating: '4/10', shortDescription: 'Furiously push your hungry hippo’s tail a million times to eat the most marbles' },
+  { title: 'Guess Who?', type: 'board', numberOfPlayers: '2', rating: '5/10', shortDescription: 'Guess who your opponents character is by questioning different characteristics and flip down your list of options until you have one left' }
+];
+console.log(games);
