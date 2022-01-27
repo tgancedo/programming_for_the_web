38a28acfba36c8f55445fde2f393ed54541776d2
Tatiana Gancedo <tgancedo@mcad.edu>
2022-01-26 21:47:51 -0500

    add window prompt and push

--- week2/exercises/arrays.js
+++ week2/exercises/arrays.js
@@ -1,2 +1,6 @@
 const myFavMovies = ['Back to the Future', 'The Fifth Element', 'Spirited Away', 'Roman Holiday', 'Coco'];
 console.log(myFavMovies);
+
+const extraMovie = window.prompt('What is your favorite movie?');
+myFavMovies.push('sdflsdjf');
+console.log('My Fav Movies updated');
