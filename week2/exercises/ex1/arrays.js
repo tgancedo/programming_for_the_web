1a213438072995b676fba3aeaa4a764e0f78f409
Tatiana Gancedo <tgancedo@mcad.edu>
2022-01-26 22:22:07 -0500

    add window alert with join

--- week2/exercises/ex1/arrays.js
+++ week2/exercises/ex1/arrays.js
@@ -4,3 +4,5 @@ console.log(myFavMovies);
 const extraMovie = window.prompt('What is your favorite movie?');
 myFavMovies.push(extraMovie);
 console.log('My Fav Movies updated');
+
+window.alert(myFavMovies.join(', '));
