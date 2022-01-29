aa4a33e0c3fd714b1f77507fc8fb53ac5a1c4702
Tatiana Gancedo <tgancedo@mcad.edu>
2022-01-29 11:47:32 -0500

    fixed last string in alert

--- week2/assignment/questions.js
+++ week2/assignment/questions.js
@@ -8,4 +8,4 @@ console.log(questions);
 const questionLength = questions.length;
 const questionIndex = Math.floor(Math.random() * questionLength);
 const answer = window.prompt(questions[questionIndex].question);
-window.alert('You answered ' + answer + '. The correct answer is ' + answer + '.');
+window.alert('You answered ' + answer + '. The correct answer is ' + questions[questionIndex].answer + '.');
