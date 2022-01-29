af92f84c4f64e9365a9b67b1bd84ca7c6eff5336
Tatiana Gancedo <tgancedo@mcad.edu>
2022-01-29 11:44:57 -0500

    create question concat structure

--- week2/assignment/questions.js
+++ week2/assignment/questions.js
@@ -1,5 +1,11 @@
 const questions = [
   { question: 'lorem?', answer: 'lorem' },
-  { question: 'lorem?', answer: 'lorem' }
+  { question: 'ipsum', answer: 'ipsum' },
+  { question: 'blue', answer: 'blue' }
 ];
 console.log(questions);
+
+const questionLength = questions.length;
+const questionIndex = Math.floor(Math.random() * questionLength);
+const answer = window.prompt(questions[questionIndex].question);
+window.alert('You answered ' + answer + '. The correct answer is ' + answer + '.');
