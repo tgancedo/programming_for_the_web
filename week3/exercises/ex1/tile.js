fb5bcde511ab5ff4356eedcdca2bc24ef2e39fd1
Tatiana Gancedo <tgancedo@mcad.edu>
2022-02-02 10:32:20 -0500

    create circle

--- week3/exercises/ex1/tile.js
+++ week3/exercises/ex1/tile.js
@@ -1,20 +1,28 @@
 function setup() {
   createCanvas(200,200);
 }
+
 function createTile() {
   translate(0, 0);
-  fill('blue');
+  fill('black');
   rect(0, 0, 200, 200);
-  stroke('white');
-  strokeWeight(5);
-  line(0, 0, 200, 200);
-  line(200, 0, 0, 200);
   noStroke();
-  fill('yellow');
-  circle(100, 50, 50);
-  circle(50, 100, 50);
-  circle(150, 100, 50);
-  circle(100, 150, 50);
+  fill('orange');
+  circle(100, 100, 200);
+  // stroke('white');
+  // strokeWeight(5);
+  // line(0, 0, 200, 200);
+  // line(200, 0, 0, 200);
+  // noStroke();
+  // fill('yellow');
+  // circle(100, 50, 50);
+  // circle(50, 100, 50);
+  // circle(150, 100, 50);
+  // circle(100, 150, 50);
+  // noFill();
+  // stroke('purple');
+  // strokeWeight(5);
+  // circle(100, 100, 100);
 }
 
 function draw() {
