6b22ae745bdd5c200adf3fece70a9aea040789bd
Tatiana Gancedo <tgancedo@mcad.edu>
2022-02-02 15:13:17 -0500

    change fill colors, created tile pattern

--- week3/exercises/ex2/pattern.js
+++ week3/exercises/ex2/pattern.js
@@ -1,25 +1,31 @@
 function setup() {
   createCanvas(600, 600);
   angleMode(DEGREES);
+  noLoop();
 }
 
-function createTile() {
-  translate(0, 0);
-  fill('black');
+function createTile(originX, originY, primaryColor, diamond) {
+  translate(originX, originY);
+  fill(primaryColor);
   noStroke();
   rect(0, 0, 200, 200);
-  fill('orange');
+  fill(237, 237, 237);
   arc(200, 200, 150, 150, 180, 270);
-  fill('yellow');
   arc(0, 200, 150, 150, 270, 0);
-  fill('green');
   arc(0, 0, 150, 150, 150, 90);
-  fill('red');
   arc(200, 0, 150, 150, 90, -90);
-  fill('white');
+  fill(diamond);
   quad(100, 70, 120, 100, 100, 130, 80, 100);
 }
 
 function draw() {
-  createTile();
+  createTile(0, 0, 'red', 'blue');
+  createTile(0, 200, 'blue', 'red');
+  createTile(0, 200, 'green', 'orange');
+  createTile(200, -400, 'orange', 'green');
+  createTile(0, 200, 'yellow', 'purple');
+  createTile(0, 200, 'purple', 'yellow');
+  createTile(200, -400, 'pink', 'brown');
+  createTile(0, 200, 'brown', 'pink');
+  createTile(0, 200, 'indigo', 'blue');
 }
