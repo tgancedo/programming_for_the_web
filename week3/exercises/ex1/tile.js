0f4bca80a854c10fed109a3b216c5478e50d3867
Tatiana Gancedo <tgancedo@mcad.edu>
2022-02-02 15:12:58 -0500

    change fill colors

--- week3/exercises/ex1/tile.js
+++ week3/exercises/ex1/tile.js
@@ -1,26 +1,22 @@
 function setup() {
   createCanvas(200,200);
   angleMode(DEGREES);
+  noLoop();
 }
 
 function createTile() {
   translate(0, 0);
-  fill('black');
+  fill('red');
   noStroke();
   rect(0, 0, 200, 200);
-  fill('orange');
+  fill(237, 237, 237);
   arc(200, 200, 150, 150, 180, 270);
-  fill('yellow');
   arc(0, 200, 150, 150, 270, 0);
-  fill('green');
   arc(0, 0, 150, 150, 150, 90);
-  fill('red');
   arc(200, 0, 150, 150, 90, -90);
-  fill('white');
   quad(100, 70, 120, 100, 100, 130, 80, 100);
 }
 
 function draw() {
   createTile();
-  noLoop();
 }
