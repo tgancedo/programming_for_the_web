8d3aabf634ce153cda6fcb533377d1339141f0ab
Tatiana Gancedo <tgancedo@mcad.edu>
2022-02-02 09:47:29 -0500

    add lines and circles

--- week3/exercises/ex1/tile.js
+++ week3/exercises/ex1/tile.js
@@ -5,6 +5,16 @@ function createTile() {
   translate(0, 0);
   fill('blue');
   rect(0, 0, 200, 200);
+  stroke('white');
+  strokeWeight(5);
+  line(0, 0, 200, 200);
+  line(200, 0, 0, 200);
+  noStroke();
+  fill('yellow');
+  circle(100, 50, 50);
+  circle(50, 100, 50);
+  circle(150, 100, 50);
+  circle(100, 150, 50);
 }
 
 function draw() {
