7990dad00aff7ffdeba795ad775853374e2678eb
Tatiana Gancedo <tgancedo@mcad.edu>
2022-02-05 14:27:11 -0500

    added x, y parameters, cleaned file

--- week3/assignment/spiro.js
+++ week3/assignment/spiro.js
@@ -1,6 +1,5 @@
-let angle = 0;
 let rotateBy = 5;
-let r, g, b;
+let r, g, b; //random colors declaration
 
 function setup() {
   createCanvas(1000, 1000);
@@ -9,32 +8,42 @@ function setup() {
   r = random(255);
   g = random(255);
   b = random(255);
+
+  //stop button
   button = createButton('stop');
   button.position(0, 0);
   button.mousePressed(noLoop);
+
+  //start button
   button = createButton('start');
   button.position(500, 0);
   button.mousePressed(loop);
 }
 
-function makeArm(rotateBy) {
+function makeArm(circleX, circleY, rotateBy) {
   let alt = Math.round(rotateBy / 360);
+  //large circle
   noFill();
   stroke(r, g, b);
   strokeWeight(1);
   line(30, 20 + alt, 85, 75);
-  ellipse(150, 150 + alt, 200, 200 - alt);
+  ellipse(circleX, circleY + alt, 200, 200 - alt);
+
+  //small square
+  rectMode(CENTER);
+  fill(0);
+  rect(0, 0, 100, 100);
 }
 
 function draw() {
   translate(500, 500);
   rotate(rotateBy);
-  makeArm(rotateBy);
+  makeArm(150, 150, rotateBy);
   rotateBy += 5;
 }
 
 function mousePressed() {
-    r = random(255);
-    g = random(255);
-    b = random(255);
+  r = random(255);
+  g = random(255);
+  b = random(255);
 }
