37c40e7a5154c773b613aac019ed2698a8e4cb78
Tatiana Gancedo <tgancedo@mcad.edu>
2022-02-16 19:02:47 -0500

    added loop

--- week5/exercises/ex2/bubble.js
+++ week5/exercises/ex2/bubble.js
@@ -1,31 +1,37 @@
-// Use a for loop in the draw function to draw a number of instances of your class on the canvas.
-
-let bubble;
+let bubbles = [];
 
 function setup () {
-  createCanvas(600, 400);
-  bubble = new Bubble();
+  createCanvas(800, 800);
+  for (let i = 0; i < 100; i++) {
+    let x = random(width);
+    let y = random(height);
+    let r = random(10, 5);
+    bubbles[i] = new Bubble(x, y, r);
+  }
 }
 
 function draw () {
-  background (0);
-  bubble.move();
-  bubble.show();
+  background (216, 255, 63);
+  for (let i = 0; i < bubbles.length; i++) {
+    bubbles[i].move();
+    bubbles[i].show();
+  }
 }
 
 class Bubble {
-  constructor() {
-    this.x = 200;
-    this.y = 150;
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
   }
   move() {
-    this.x = this.x + random(-5, 5);
-    this.y = this.y + random(-5, 5);
+    this.x = this.x + random(-3, 3);
+    this.y = this.y + random(-3, 3);
 }
   show() {
-    stroke(255);
-    strokeWeight(4);
+    stroke(0);
+    strokeWeight(1);
     noFill();
-    ellipse(this.x, this.y, 24, 24);
+    ellipse(this.x, this.y, this.r * 12);
   }
 }
