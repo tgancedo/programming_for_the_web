f6ff87c50ff5192f701e620e7021ef15dbbe3723
Tatiana Gancedo <tgancedo@mcad.edu>
2022-02-17 18:47:53 -0500

    add flip

--- week5/assignment/game.js
+++ week5/assignment/game.js
@@ -1,4 +1,6 @@
 let myCard;
+const DOWN = 'down';
+const UP = 'up';
 
 function setup () {
   createCanvas(1080, 1080);
@@ -16,20 +18,36 @@ class Card {
     this.y = 100;
     this.width = 200;
     this.height = 200;
+    this.face = DOWN;
     this.show();
   }
 
   show () {
-    fill(255);
-    rect(this.x, this.y, this.width, this.height);
+    if(this.face === DOWN) {
+      fill(255);
+      rect(this.x, this.y, this.width, this.height);
+    } else {
+      fill(100);
+      rect(this.x, this.y, this.width, this.height);
+    }
   }
 
   didHit (mouseX, mouseY) {
     if (mouseX >= this.x && mouseX <= this.x + this.width &&
         mouseY >= this.y && mouseY <= this.y + this.height) {
+          this.flip();
           return true;
         } else {
           return false;
         }
+  }
+
+  flip () {
+    if (this.face === DOWN) {
+        this.face = UP;
+    } else {
+      this.face = DOWN;
+    }
+    this.show()
   }
 }
