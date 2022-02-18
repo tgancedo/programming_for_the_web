dfb874dcf4aac2fb9ea89d5ad3c160539148e73f
Tatiana Gancedo <tgancedo@mcad.edu>
2022-02-17 19:22:39 -0500

    loadimages, setup loop of cards

--- week5/assignment/game.js
+++ week5/assignment/game.js
@@ -1,21 +1,54 @@
 let myCard;
 const DOWN = 'down';
 const UP = 'up';
+let startingX = 30;
+let startingY = 200;
+let cards = [];
+const gameState = {
+
+};
+let cardfaceArray = [];
+
+function preload() {
+  cardback = loadImage('images/card-back.png');
+  cardfaceArray = [
+    loadImage('images/avocado.png'),
+    loadImage('images/cucumber.png'),
+    loadImage('images/salmon-avocado.png'),
+    loadImage('images/salmon-cucumber.png'),
+    loadImage('images/salmon.png'),
+    loadImage('images/tuna-avocado.png'),
+    loadImage('images/tuna-cucumber.png'),
+    loadImage('images/tuna.png')
+  ]
+}
 
 function setup () {
   createCanvas(1080, 1080);
-  background(0);
-  myCard = new Card();
+  background(255);
+  for (let j = 0; j < 4; j++) {
+    for (let i = 0; i < 4; i++) {
+      cards.push(new Card(startingX, startingY));
+      startingX += 225;
+    }
+    startingY += 225;
+    startingX = 30;
+  }
 }
 
 function mousePressed () {
-  console.log(myCard.didHit(mouseX, mouseY));
+  for (let k = 0; k < cards.length; k++) {
+    if(cards[k].didHit(mouseX, mouseY)) {
+      console.log('flipped');
+      return false;
+    }
+  }
 }
 
 class Card {
-  constructor () {
-    this.x = 100;
-    this.y = 100;
+  constructor (x, y) {
+    this.x = x;
+    this.y = y;
     this.width = 200;
     this.height = 200;
     this.face = DOWN;
@@ -24,8 +57,9 @@ class Card {
 
   show () {
     if(this.face === DOWN) {
-      fill(255);
+      fill(0);
       rect(this.x, this.y, this.width, this.height);
+      image(cardback, this.x, this.y);
     } else {
       fill(100);
       rect(this.x, this.y, this.width, this.height);
