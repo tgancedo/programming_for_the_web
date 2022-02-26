d3b500277236e980f0c55d98f17dc8292f794195
Tatiana Gancedo <tgancedo@mcad.edu>
2022-02-26 09:35:37 -0500

    update facts

--- week6/assignment/llamas.js
+++ week6/assignment/llamas.js
@@ -1,15 +1,16 @@
+//facts from https://www.discovery.com/nature/llama-mia--10-fun-facts-about-llamas-pictures
 new Vue({
   el: "#app",
   data: {
-      llamaPoop: "poop.",
-      llamaCaca: "poop.",
-      llamaPopo: "poop.",
-      llamaDodo: "poop.",
-      llamaLolo: "poop.",
-      llamaCoco: "poop.",
-      llamaFofo: "poop.",
-      llamaPipi: "poop.",
-      llamaTipi: "poop.",
-      llamaKipi: "poop."
+      llamaSmart: "Llamas are smart.",
+      llamaLife: "A llama's lifespan is 20 years.",
+      llamaSocial: "Llamas are very social.",
+      llamaTall: "Llamas can grow as much as 6 feet tall.",
+      llamaHum: "Llamas can hum.",
+      llamaTherapy: "Llamas are used as therapy animals.",
+      llamaYarn: "Yarn made from llama fiber is extremely versatile.",
+      llamaSpit: "Llamas can shoot green spit up to 10 feet away.",
+      llamaVeg: "Llamas are vegetarians.",
+      llamaCamelid: "Llamas are members of the camelid family."
   },
 });
