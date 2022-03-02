57dd3bf384141fea20e5081022119c2a79506415
Tatiana Gancedo <tgancedo@mcad.edu>
2022-03-02 18:29:46 -0500

    create script with links and titles

---
+++ week7/exercises/ex1/script.js
@@ -0,0 +1,10 @@
+Vue.createApp({
+  data() {
+    return {
+      llamas: "https://en.wikipedia.org/wiki/Llama",
+      llamasTitle: "Llamas are cool",
+      alpacas: "https://en.wikipedia.org/wiki/Alpaca",
+      alpacasTitle: "Alpacas are cooler"
+    }
+  }
+}).mount('#app')
