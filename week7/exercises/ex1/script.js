9def03abdef2f95e77fd062c56c2731955680fe7
Tatiana Gancedo <tgancedo@mcad.edu>
2022-03-02 20:49:51 -0500

    updated attribute binding names

--- week7/exercises/ex1/script.js
+++ week7/exercises/ex1/script.js
@@ -1,9 +1,9 @@
 Vue.createApp({
   data() {
     return {
-      llamas: "https://en.wikipedia.org/wiki/Llama",
+      llamasLink: "https://en.wikipedia.org/wiki/Llama",
       llamasTitle: "Llamas are cool",
-      alpacas: "https://en.wikipedia.org/wiki/Alpaca",
+      alpacasLink: "https://en.wikipedia.org/wiki/Alpaca",
       alpacasTitle: "Alpacas are cooler",
       baseStyle: {
         color: "red",
