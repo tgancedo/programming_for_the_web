44f91221d650ce04a1c0f0acf6508e69ac47b047
Tatiana Gancedo <tgancedo@mcad.edu>
2022-03-09 21:10:48 -0500

    update data

--- week8/exercises/ex1/script.js
+++ week8/exercises/ex1/script.js
@@ -1,7 +1,22 @@
 Vue.createApp({
   data() {
     return {
-      
+      firstName: '',
+      lastName: '',
+      street: '',
+      state: 'MN',
+      colors: [
+        {name: 'White', value: 'white'},
+        {name: 'Beige', value: 'beige'},
+        {name: 'Fawn (light, medium, dark)', value: 'fawn'},
+        {name: 'Brown (light, medium, dark)', value: 'brown'},
+        {name: 'brown'},
+        {name: 'Bay Black', value: 'bay-black'},
+        {name: 'True Black', value: 'true-black'},
+        {name: 'Rose Gray (light, medium, dark)', value: 'rose-gray'},
+        {name: 'Silver Gray (light, medium, dark)', value: 'silver-gray'}
+      ],
+      status: ''
     }
   }
 }).mount('#alpacaForm')
