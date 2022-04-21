ac9ed8e156b84916aaf2f1af1ffba155c6d3eccd
Tatiana Gancedo <tgancedo@mcad.edu>
2022-04-21 16:41:46 -0400

    upload exercise 13.1

---
+++ week13/exercise/main.js
@@ -0,0 +1,40 @@
+var pos;
+
+function setup () {
+  createCanvas(800, 800)
+  angleMode(DEGREES)
+
+  pos = 0.0 // position as float value in decimals
+  inc = 1.0  // increase position in angle
+
+  pos2 = 0.0
+  inc2 = 1.0
+}
+
+function draw () {
+  
+
+  clear()
+  background(30)
+  noFill()
+  
+  translate(width/2, height/2)
+
+  var mySinVal = sin(pos)
+  var mySinVal2 = sin(pos2)
+
+  amplified = mySinVal * 200
+  amplified2 = mySinVal2 * 100
+
+  for (x = 0; x <= 360; x = x + 5) {
+    stroke(192, 255, 181)
+    curve(0, 0, x, amplified, 400, 400, 0, 0)
+    stroke(255, 181, 238)
+    ellipse(0, amplified2, amplified2 - x, amplified)
+    rotate(x)
+  }
+
+  pos = pos + inc
+  pos2 = pos2 + inc
+
+}
